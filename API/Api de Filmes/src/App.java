d140fee292dd28623cb69f8232f33f89f804378e
Julia Farias da Rocha <jufarias20002@gmail.com>
2022-07-24 17:16:09 -0300

    Aula 2

--- API/Api de Filmes/src/App.java
+++ API/Api de Filmes/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -7,31 +9,37 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
-        // fazer a conexão HTTP e buscar os 250 filmes
-        String url = "https://api.themoviedb.org/3/trending/movie/week?api_key=ee4b2ac5da8f5be482301468e7d0292f";
-        var client = HttpClient.newHttpClient();
-
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
+       //String url="https://api.themoviedb.org/3/movie/550?api_key=ee4b2ac5da8f5be482301468e7d0292f";
         URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = (String) response.body();
-        // System.out.println(body);
+        String body = response.body();
 
-        // pegar dados da api do the moviedb(titulo,imagem,classificação)
+        // extrair só os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        List<Map<String, String>> listaFilme = parser.parse(body);
-       // System.out.println(listaFilme.size());
-       
-        for (Map<String, String> filme : listaFilme) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("backdrop_path"));
-            System.out.println(filme.get("vote_average"));
+        // exibir e manipular os dados 
+        var geradora = new GeradoradeFigurinhas();
+        for (Map<String,String> filme : listaDeFilmes) {
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.criarFigurinha(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
         }
-
     }
 }
